07caa6d119ef63a7d870044de01ba0e89f6d5692
André Bessa <andrebessax@gmail.com>
2022-09-30 17:08:02 -0300

    Adicionar arquivos de projeto.

---
+++ UsandoRedis/Model/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoRedis.Model
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $" === Info ===" +
+                   $"Código = {this.Id}\n" +
+                   $"Nome = {this.Name}\n" +
+                   $"Email = {this.Email}";
+        }
+    }
+}
---
+++ UsandoRedis/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using UsandoRedis.Connection;
+using UsandoRedis.Model;
+
+Console.WriteLine(" === Usando o Redis ===");
+
+Console.WriteLine("Salvando um objeto no Cache.");
+Random random = new Random();   
+Client cli = new Client();
+cli.Id = random.Next(0, 100);
+cli.Name = "André Silva";
+cli.Email = "andre@email.com";
+SaveData(cli);
+
+Console.WriteLine("Lendo um objeto no Cache.");
+ReadData(cli.Id);
+
+void ReadData(int id)
+{
+    var cache = RedisConnection.Connection.GetDatabase();
+    var value = cache.StringGet($"Cli_{id}");
+    var cli = JsonConvert.DeserializeObject<Client>(value.ToString());
+    Console.WriteLine(cli.ToString());    
+}
+
+void SaveData(Client cli)
+{  
+    var json = JsonConvert.SerializeObject(cli, Formatting.Indented);  
+    var cache = RedisConnection.Connection.GetDatabase();  
+    cache.StringSet($"Cli_{cli.Id}",json);   
+}
